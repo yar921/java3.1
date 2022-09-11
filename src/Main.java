9af2171d6f0d608b05b7fefbd75d33453faef797
Yaroslav.S <yar921@yandex.ru>
2022-09-11 20:54:01 +0300

    new

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int amount = 100; //сумма на которую купили билеты
+        int bonus = 20; //сумма на 1 бонусную милю
+
+        System.out.println("Начислено бонусных миль: " + (amount / bonus));
+    }
+}
